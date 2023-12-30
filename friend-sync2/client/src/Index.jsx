f4e93dc0a255a25071ae31a4882458249d83e6ac
Hikmat Ali <hikmat.aliyevm@gmail.com>
2023-12-30 19:07:08 +0400

    Create comment and like logic

--- friend-sync2/client/src/Index.jsx
+++ friend-sync2/client/src/Index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import './Index.css'
 import { useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom';
--- friend-sync2/client/src/Post.jsx
+++ friend-sync2/client/src/Post.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useSyncExternalStore } from 'react';
 import axios from 'axios';
 const API_BASE = 'http://localhost:3000'
 import './Post.css'
@@ -9,6 +9,13 @@ const Post = ({userInfo}) => {
   const [post, setPost] = useState('');
   const [error, setError] = useState(null);
   const [user, setUserInfo] = useState(userInfo);
+  const [commentInputToggle, setToggle] = useState(false);
+  const [commentText, setCommentText] = useState(" ");
+  const [showLikeList, setShowLikeList] = useState(false);
+  const [likeList, setLikeList] = useState([]);
+  const [showCommentList, setShowCommentList] = useState(false);
+  const [commentList, setCommentList] = useState([]);
+
   //get all posts
   useEffect(() => {
     const fetchData = async () => {
@@ -109,6 +116,38 @@ const Post = ({userInfo}) => {
     return likes.some(like => like.email == user.email)
   }
 
+  async function handleCommentPost(postId) {
+    setCommentText('');
+    try{
+      const response = await axios.post(`${API_BASE}/post/comment`, {
+        postId, user, commentText
+      },{
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      const data = response.data;
+      setPosts(data);
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  function handleCommentInput(post) {
+    post.showCommentInput = true;
+    setToggle(!commentInputToggle);
+  }
+
+  function handleLikeList(post) {
+    setLikeList(post.likes)
+    setShowLikeList(true)
+  }
+
+  function handleCommentList(post) {
+    setCommentList(post.comments)
+    setShowCommentList(true)
+  }
+
   return (
     <div>
       <div className='createPost'>
@@ -121,20 +160,37 @@ const Post = ({userInfo}) => {
       </div>
 
       {posts && posts.map((post, index) => (
-        
         <div key={index}>
+
           <div className='post-header'>
             <h1>{post.username}</h1>
-            <p>{formatDistanceToNow(post.date, {addSuffix: true})}</p>
-            <button onClick={() => handlePostDelete(post._id)}>Delete</button>
+            <p>posted {formatDistanceToNow(post.date, {addSuffix: true})}</p>
+            {/* show delete btn if the post belongs to the current user */}
+            {post.user._id == user._id ? <button onClick={() => handlePostDelete(post._id)}>Delete</button> : null}
           </div>
 
           <p>{post.text}</p>
 
           <div>
-            <a href="">{post.like_number} likes</a>
-            <a href="">comments</a>
+            <button onClick={() => handleLikeList(post)}>{post.like_number} likes</button>
+            <button onClick={() => handleCommentList(post)}>{post.comment_number} comments</button>
           </div>
+
+          {showLikeList && <div className='list-of-likes'>
+            {likeList.length > 0 ? likeList.map((like, index) => (
+              <p key={index}>{like.username}</p>
+            )) : <p key={index}>no likes</p>}
+          </div>} 
+
+          {showCommentList && <div className='list-of-comments'>
+            {commentList.length > 0 ? commentList.map((comment, index) => (
+              <div key={index}>
+                <h3>{comment.username}</h3>
+                <p>posted {formatDistanceToNow(comment.date, {addSuffix: true})}</p>
+                <p>{comment.text}</p>
+              </div>
+            )) : <p key={index}>no comments</p>}
+          </div>} 
 
             
           <div>
@@ -142,9 +198,30 @@ const Post = ({userInfo}) => {
             <button onClick={() => handlePostUnlike(post._id)} className='liked-button'>Like</button> : 
             <button  onClick={() => handlePostLike(post._id)}>Like</button>}
 
+            <button onClick={() => handleCommentInput(post)}>Comment</button>
 
-            <button>Comment</button>
+            <button className='see-all-comments' onClick={() => handleCommentList(post)}>See all comments</button>
+              
+              {/* limit number of shown comments to 3 */}
+            {post.comments.slice(-3).map((comment, id) => (
+                <div key={id}>
+                  <h3>{comment.username}</h3>
+                  <p>posted {formatDistanceToNow(comment.date, {addSuffix: true})}</p>
+                  <p>{comment.text}</p>
+                  {/* show delete btn if the comment belongs to the current user */}
+                  {comment.email == user.email ? <button>Delete</button> : null}
+                </div>
+              ))}
+
+            {post.showCommentInput &&
+            <div> 
+              <input type="text" placeholder='Write a comment...' value={commentText} onChange= {(e) => setCommentText(e.target.value)}/>
+              <button onClick={() => handleCommentPost(post._id)}
+                className={commentText.trim() == "" ? 'disabled-postBtn' : ''}
+              >Post</button>
+            </div>}
           </div>
+
         </div>
       ))}
 </div>
