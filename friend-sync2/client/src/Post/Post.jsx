4cb76a6e9b36aa366af65dda3ef2cdce73391109
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-01-09 12:22:25 +0100

    Add loader animation to improve UI

--- friend-sync2/client/src/Post/Post.jsx
+++ friend-sync2/client/src/Post/Post.jsx
@@ -7,12 +7,15 @@ import postLogo from '../images/sendLogo.svg';
 import { useNavigate } from 'react-router-dom';
 import { handleProfilePage } from '../Profiles/Profile';
 import { convertToBase64 } from '../Picture/Picture';
+import Loader from '../Loader/Loader';
+
 // import HeartButton from '../HeartButton/HeartBtn';
 
 // eslint-disable-next-line react/prop-types
 const Post = ({userInfo, profileInfo}) => {
   const [posts, setPosts] = useState(null);
-  const [post, setPost] = useState('');
+  const [createdPost, setCreatedPost] = useState('');
+  const [userPost, setUserPost] = useState(null);
   const user = userInfo;
   // set postOwner to profile data if we are in friend page
   const postOwner = profileInfo ? profileInfo : userInfo;
@@ -23,11 +26,13 @@ const Post = ({userInfo, profileInfo}) => {
   const [showCommentList, setShowCommentList] = useState(false);
   const [postImage, setPostImage] = useState(null);
   const imageInputRef = useRef(null);
-  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const navigate = useNavigate();
 
   //get all posts
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const response = await axios.post(`${API_BASE}/post/get-posts`, {
@@ -40,6 +45,8 @@ const Post = ({userInfo, profileInfo}) => {
         setPosts(response.data);
       } catch (err) {
         console.log(err);
+      }finally {
+        setLoading(false);
       }
     };
   
@@ -50,7 +57,7 @@ const Post = ({userInfo, profileInfo}) => {
       e.preventDefault();
       try{
         const response = await axios.post(`${API_BASE}/post/submit`, {
-          post, user, postImage
+          createdPost, user, postImage
         }, {
           headers: {
             'Content-Type': 'application/json',
@@ -59,7 +66,7 @@ const Post = ({userInfo, profileInfo}) => {
 
         const data = response.data;
         setPosts(data);
-        setPost('');
+        setCreatedPost('');
         setPostImage(null)
       }catch(err){
         console.log(err)
@@ -136,11 +143,6 @@ const Post = ({userInfo, profileInfo}) => {
     }
   }
 
-  async function handleCommentList(post) {
-    setComments(post.comments)
-    setShowCommentList(true);
-  }
-
   function handleCommentInput(post) {
     post.showCommentInput = true;
     setToggle(!commentInputToggle);
@@ -151,6 +153,11 @@ const Post = ({userInfo, profileInfo}) => {
     setShowLikeList(true)
   }
 
+  function handleCommentList(post) {
+    setUserPost(post);
+    setShowCommentList(true)
+  }
+
   async function handleCommentDelete(postId, commentId) {
     try{
       const response = await axios.post(`${API_BASE}/post/comment/delete`, {
@@ -158,10 +165,16 @@ const Post = ({userInfo, profileInfo}) => {
       },{
         headers: {
           'Content-Type': 'application/json',
-        },
+        }
       })
       const data = response.data;
+      if(userPost){
+        const newCommentsArray = userPost.comments.filter(comment => comment._id != commentId);
+        userPost.comments = newCommentsArray;
+        setUserPost(userPost);
+      }
       setPosts(data);
+      
     }catch(err){
       console.log(err)
     }
@@ -179,147 +192,155 @@ const Post = ({userInfo, profileInfo}) => {
   }
 
   return (
-    <div className='post-container'>
-      {!profileInfo && 
-      <div className='create-post-container'>
-        <form onSubmit={handlePostSubmit}>
-          <textarea type="text" placeholder="What's on your mind?" 
-          value={post} onChange= {(e) => setPost(e.target.value)}/>
-        </form>
+    <div className='post-page'>
+      {loading ? <div className="loading-spinner"><Loader /></div>
+      : <div className='post-container'>
+        {!profileInfo && 
+        <div className='create-post-container'>
+          <form onSubmit={handlePostSubmit}>
+            <textarea type="text" placeholder="What's on your mind?" 
+            value={createdPost} onChange= {(e) => setCreatedPost(e.target.value)}/>
+          </form>
 
-         <div className='post-create-image-container'>
+          <div className='post-create-image-container'>
 
-         {postImage && <span onClick={() => setPostImage(null)} className="material-symbols-outlined">close</span>}
+          {postImage && <span onClick={() => setPostImage(null)} className="material-symbols-outlined">close</span>}
 
-          <img src={postImage} className='post-create-image'/>
+            <img src={postImage} className='post-create-image'/>
 
-        </div> 
-        <input ref={imageInputRef} type='file' label ='Image' name='post_picture' id='post-pic-upload'
-            accept='.jpeg, .png, .jpg' onChange={(e) => handleFileUpload(e)}/>
+          </div> 
+          <input ref={imageInputRef} type='file' label ='Image' name='post_picture' id='post-pic-upload'
+              accept='.jpeg, .png, .jpg' onChange={(e) => handleFileUpload(e)}/>
 
-        <div className='upload-and-image-button-container'>
-          <label htmlFor="post-pic-upload">
-            <span className="material-symbols-outlined">
-            image
-            </span>
-          </label>
+          <div className='upload-and-image-button-container'>
+            <label htmlFor="post-pic-upload">
+              <span className="material-symbols-outlined">
+              image
+              </span>
+            </label>
 
-          <button onClick={handlePostSubmit} className={post.trim() == "" ? 'disabled-postBtn' : 'postBtn'}  type='submit'>
-            <img className='postLogo .material-symbols-outlined' src={postLogo} />
-          </button>
-        </div>
-
-      </div>}
-
-      {posts && posts.map((post, index) => (
-        <div className='single-post-container' key={index}>
-
-          <div className='post-header'>
-            <div>
-              <h2>{post.username}</h2>
-              <p>posted {formatDistanceToNow(post.date, {addSuffix: true})}</p>
-            </div>
-            {/* show delete btn if we are not in friend's page */}
-            {!profileInfo ? <button className='post-delete-button' onClick={() => handlePostDelete(post._id)}>
-            <span id='delete-logo' className="material-symbols-outlined"> delete </span>
-              </button> : null}
+            <button onClick={handlePostSubmit} className={createdPost.trim() == "" ? 'disabled-postBtn' : 'postBtn'}  type='submit'>
+              <img className='postLogo .material-symbols-outlined' src={postLogo} />
+            </button>
           </div>
 
-          <p className='post-text'>{post.text}</p>
-          <img className='single-post-picture' src={post.post_picture}/>
+        </div>}
 
-          <div className='like-comment-list-container'>
-            { post.like_number > 0 ? 
-            <button onClick={() => handleLikeList(post)}>
-              {post.like_number} {post.like_number ==1 ? 'like' : 'likes'}
-            </button> : <p></p>}
-              
-            { post.comment_number > 0 ? 
-            <button className='comment-number' onClick={() => handleCommentList(post)}>
-              {post.comment_number} {post.comment_number == 1 ? 'comment' : 'comments'}
-            </button> : <p></p>}
-          </div>
+        {posts && posts.map((post, index) => (
+          <div className='single-post-container' key={index}>
 
-          {showLikeList && <div className='list-of-likes'>
-            {likeList.length > 0 ? likeList.map((like, index) => (
-              <p key={index} 
-                onClick={() => {
-                  handleProfilePage(like.userId, navigate)
-                  setShowLikeList(false)
-                  }}>
-                {like.username}
-                </p>
-            )) : <p key={index}>no likes</p>}
-          </div>} 
-
-          {showCommentList && <div className='list-of-comments'>
-            {comments.length > 0 ? comments.map((comment, index) => (
-              <div key={index}>
-                <h3 onClick={() => {handleProfilePage(comment.userId, navigate)
-                                    setShowCommentList(false)}}>
-                  {comment.username}
-                </h3>
-                {/* show delete btn if the comment belongs to the current user */}
-                {comment.email == user.email ? 
-                <button onClick={() => {handleCommentDelete(post._id, comment._id)}}>
-                    Delete
-                </button> : null}
-                <p>posted {formatDistanceToNow(comment.date, {addSuffix: true})}</p>
-                <p>{comment.text}</p>
+            <div className='post-header'>
+              <div>
+                <h2>{post.username}</h2>
+                <p>posted {formatDistanceToNow(post.date, {addSuffix: true})}</p>
               </div>
-            )) : <p key={index}>No comments</p>}
-          </div>} 
-
-          <div>
-            <div className='like-comment-btn-container'>
-              {isPostLiked(post.likes) ? 
-              <button onClick={() => handlePostUnlike(post._id)} className='liked-button'>
-                 <span id='like-logo' className="material-symbols-outlined"> thumb_up </span>
-                  Like
-              </button> : <button  onClick={() => handlePostLike(post._id)}>
-                 <span className="material-symbols-outlined"> thumb_up </span>
-                  Like
-              </button> }
-
-              <button onClick={() => handleCommentInput(post)}>
-              <span id='comment-logo' className="material-symbols-outlined"> mode_comment </span> Comment</button>
+              {/* show delete btn if we are not in friend's page */}
+              {!profileInfo ? <button className='post-delete-button' onClick={() => handlePostDelete(post._id)}>
+              <span id='delete-logo' className="material-symbols-outlined"> delete </span>
+                </button> : null}
             </div>
+
+            <p className='post-text'>{post.text}</p>
+            <img className='single-post-picture' src={post.post_picture}/>
+
+            <div className='like-comment-list-container'>
+              { post.like_number > 0 ? 
+              <button onClick={() => handleLikeList(post)}>
+                {post.like_number} {post.like_number ==1 ? 'like' : 'likes'}
+              </button> : <p></p>}
                 
-            {post.comments.length > 3 && 
-            <button className='see-all-comments' onClick={() => handleCommentList(post)}>
-              See all comments
-            </button>}
-              
-              {/* limit number of shown comments to 3 */}
-            {post.comments.slice(-3).map((comment, id) => (
-                <div className='comment-container' key={id}>
-                  <div className='single-comment-header'>
-                    <h3 onClick={() => handleProfilePage(comment.userId, navigate)} className='comment-username'>
-                      {comment.username}
-                    </h3>
-                     {/* show delete btn if the comment belongs to the current user */}
-                     {comment.email == user.email ? <button 
-                      onClick={() => handleCommentDelete(post._id, comment._id)}>
-                        <span className="material-symbols-outlined"> delete </span>
-                      </button> : null}
-                  </div>
-                  <p className='comment-date'>{formatDistanceToNow(comment.date)} ago</p>
+              { post.comment_number > 0 ? 
+              <button className='comment-number' onClick={() => handleCommentList(post)}>
+                {post.comment_number} {post.comment_number == 1 ? 'comment' : 'comments'}
+              </button> : <p></p>}
+            </div>
+
+            {showLikeList && <div className='list-of-likes'>
+              {likeList.length > 0 ? likeList.map((like, index) => (
+                <p key={index} 
+                  onClick={() => {
+                    handleProfilePage(like.userId, navigate)
+                    setShowLikeList(false)
+                    }}>
+                  {like.username}
+                  </p>
+              )) : <p key={index}>no likes</p>}
+            </div>} 
+
+            {showCommentList && <div className='list-of-comments'>
+
+              <span className="material-symbols-outlined" onClick={() => {
+                      setShowCommentList(false)
+                      setUserPost(null)
+                    }}>close</span>
+
+              {userPost.comments.length > 0 ? userPost.comments.map((comment, index) => (
+                <div key={index}>
+
+                  <h3 onClick={() => {handleProfilePage(comment.userId, navigate)
+                                      setShowCommentList(false)}}>
+                    {comment.username}
+                  </h3>
+                  {/* show delete btn if the comment belongs to the current user */}
+                  {comment.email == user.email ? 
+                  <button onClick={() => {handleCommentDelete(userPost._id, comment._id)}}>
+                      Delete
+                  </button> : null}
+                  <p>posted {formatDistanceToNow(comment.date, {addSuffix: true})}</p>
                   <p>{comment.text}</p>
                 </div>
-              ))}
+              )) : <p key={index}>No comments</p>}
+            </div>} 
 
-            {post.showCommentInput &&
-            <div> 
-              <input type="text" placeholder='Write a comment...' value={commentText} onChange= {(e) => setCommentText(e.target.value)}/>
-              <button onClick={() => handleCommentPost(post._id)}
-                className={commentText.trim() == "" ? 'disabled-postBtn' : ''}
-              >Post</button>
-            </div>}
+            <div>
+              <div className='like-comment-btn-container'>
+                {isPostLiked(post.likes) ? 
+                <button onClick={() => handlePostUnlike(post._id)} className='liked-button'>
+                  <span id='like-logo' className="material-symbols-outlined"> thumb_up </span>
+                    Like
+                </button> : <button  onClick={() => handlePostLike(post._id)}>
+                  <span className="material-symbols-outlined"> thumb_up </span>
+                  Like
+                </button> }
+
+                <button onClick={() => handleCommentInput(post)}>
+                <span id='comment-logo' className="material-symbols-outlined"> mode_comment </span> Comment</button>
+              </div>
+                  
+              {post.comments.length > 3 && <button className='see-all-comments' onClick={() => handleCommentList(post)}>
+                See all comments</button>}
+                
+                {/* limit number of shown comments to 3 */}
+              {post.comments.slice(-3).map((comment, id) => (
+                  <div className='comment-container' key={id}>
+                    <div className='single-comment-header'>
+                      <h3 onClick={() => handleProfilePage(comment.userId, navigate)} className='comment-username'>
+                        {comment.username}
+                      </h3>
+                      {/* show delete btn if the comment belongs to the current user */}
+                      {comment.email == user.email ? <button 
+                        onClick={() => handleCommentDelete(post._id, comment._id)}>
+                          <span className="material-symbols-outlined"> delete </span>
+                        </button> : null}
+                    </div>
+                    <p className='comment-date'>{formatDistanceToNow(comment.date)} ago</p>
+                    <p>{comment.text}</p>
+                  </div>
+                ))}
+
+              {post.showCommentInput &&
+              <div> 
+                <input type="text" placeholder='Write a comment...' value={commentText} onChange= {(e) => setCommentText(e.target.value)}/>
+                <button onClick={() => handleCommentPost(post._id)}
+                  className={commentText.trim() == "" ? 'disabled-postBtn' : ''}
+                >Post</button>
+              </div>}
+            </div>
+
           </div>
-
-        </div>
-      ))}
-</div>
+        ))}
+      </div> }
+    </div>
   )
 };
 
