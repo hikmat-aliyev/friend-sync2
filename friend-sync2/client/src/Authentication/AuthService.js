0538dc730063ef36c657e6e1b0adf4df79b824a1
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-09-25 17:27:49 +0400

    Change api url

--- friend-sync2/client/src/Authentication/AuthService.js
+++ friend-sync2/client/src/Authentication/AuthService.js
@@ -1,43 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const API_BASE = 'https://friend-sync-server-production.up.railway.app'
+export const API_BASE = "https://friendsync-server.onrender.com";
 
 const AuthService = {
   login: async (email, password) => {
     try {
-      const response = await axios.post(`${API_BASE}/user/sign-in`, {
-        email,
-        password,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        `${API_BASE}/user/sign-in`,
+        {
+          email,
+          password,
         },
-      });
-  
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       const data = response.data;
-  
+
       if (!data || !data.token) {
-        throw new Error('Authentication failed');
+        throw new Error("Authentication failed");
       }
-      
-      localStorage.setItem('token', data.token);
+
+      localStorage.setItem("token", data.token);
       return data;
     } catch (error) {
-      console.error('Error during authentication:', error.message);
+      console.error("Error during authentication:", error.message);
       throw error;
     }
   },
 
   logout: () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
   },
 
   getToken: () => {
-    return localStorage.getItem('token');
+    return localStorage.getItem("token");
   },
 
   removeToken: () => {
-    localStorage.removeItem('token')
+    localStorage.removeItem("token");
   },
 
   getUserInfo: async (jwt) => {
@@ -47,100 +51,111 @@ const AuthService = {
         {},
         {
           headers: {
-            'Authorization': `Bearer ${jwt}`,
-            'Content-Type': 'application/json',
+            Authorization: `Bearer ${jwt}`,
+            "Content-Type": "application/json",
           },
         }
       );
-  
+
       const userData = response.data;
-  
+
       if (!userData) {
-        throw new Error('Failed to fetch user information');
+        throw new Error("Failed to fetch user information");
       }
-  
+
       return userData;
     } catch (error) {
-      console.error('Error fetching user information:', error.message);
+      console.error("Error fetching user information:", error.message);
       throw error;
     }
   },
 
   signUp: async (firstName, lastName, email, birthDate, password) => {
     try {
-      const response = await axios.post(`${API_BASE}/user/sign-up`, {
-        firstName,
-        lastName,
-        birthDate,
-        email,
-        password,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        `${API_BASE}/user/sign-up`,
+        {
+          firstName,
+          lastName,
+          birthDate,
+          email,
+          password,
         },
-      });
-  
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       const data = response.data;
-  
+
       if (!data || !data.token) {
-        throw new Error('Authentication failed');
+        throw new Error("Authentication failed");
       }
-  
-      localStorage.setItem('token', data.token);
+
+      localStorage.setItem("token", data.token);
       return data;
     } catch (error) {
-      console.error('Error during authentication:', error.message);
+      console.error("Error during authentication:", error.message);
       throw error;
     }
   },
 
   googleSignIn: async (email) => {
     try {
-      const response = await axios.post(`${API_BASE}/user/google/sign-in`, {
-       email
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        `${API_BASE}/user/google/sign-in`,
+        {
+          email,
         },
-      });
-  
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       const data = response.data;
       if (!data.token) {
-        throw new Error('Authentication failed');
+        throw new Error("Authentication failed");
       }
-  
-      localStorage.setItem('token', data.token);
-;
+
+      localStorage.setItem("token", data.token);
       return data;
     } catch (error) {
-      console.error('Error during authentication:', error.message);
+      console.error("Error during authentication:", error.message);
       throw error;
     }
   },
 
   googleSignUp: async (userData) => {
     try {
-      const response = await axios.post(`${API_BASE}/user/google/sign-up`, {
-       userData
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        `${API_BASE}/user/google/sign-up`,
+        {
+          userData,
         },
-      });
-  
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       const data = response.data;
       if (!data.token) {
-        console.log('no user')
-        throw new Error('Authentication failed');
+        console.log("no user");
+        throw new Error("Authentication failed");
       }
-  
-      localStorage.setItem('token', data.token);
+
+      localStorage.setItem("token", data.token);
       return data;
     } catch (error) {
-      console.error('Error during authentication:', error.message);
+      console.error("Error during authentication:", error.message);
       throw error;
     }
   },
-}
+};
 
 export default AuthService;
--- friend-sync2/server/server.js
+++ friend-sync2/server/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-require('dotenv').config();
-const bodyParser = require('body-parser')
-const cors = require('cors');
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
- //limit to 5MB 
+//limit to 5MB
 const bodyParserOptions = {
-  limit: '5mb',
+  limit: "5mb",
 };
 
 // Use body-parser middleware with the specified options
@@ -17,6 +17,7 @@ server.use(bodyParser.urlencoded({ extended: true, ...bodyParserOptions }));
 server.use(express.json());
 server.use(cors());
 //set up mongoose
+
 const mongoose = require("mongoose");
 const mongoDb = process.env.DATABASE_STRING;
 
@@ -24,17 +25,17 @@ mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
-const userRouter = require('./routes/userRouter');
-const postRouter = require('./routes/postRouter');
-const profileRouter = require('./routes/profileRouter');
-const findRouter = require('./routes/findRouter');
-const pictureRouter = require('./routes/pictureRouter');
+const userRouter = require("./routes/userRouter");
+const postRouter = require("./routes/postRouter");
+const profileRouter = require("./routes/profileRouter");
+const findRouter = require("./routes/findRouter");
+const pictureRouter = require("./routes/pictureRouter");
 
-server.use('/user', userRouter);
-server.use('/post', postRouter);
-server.use('/profiles', profileRouter);
-server.use('/find', findRouter);
-server.use('/picture', pictureRouter);
+server.use("/user", userRouter);
+server.use("/post", postRouter);
+server.use("/profiles", profileRouter);
+server.use("/find", findRouter);
+server.use("/picture", pictureRouter);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
